fd05273e50e236defd9a60f6bb01d53f8b16d9f6
Hennadii <82006228+HennadiiZ@users.noreply.github.com>
2022-07-10 14:13:21 +0300

    my solution/ all btns work. click out of the modal and close

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 'use strict';
 
 const modal = document.querySelector('.modal');
-const showModal = document.querySelector('.show-modal');
+const showModalbtns = document.querySelectorAll('.show-modal');
 const closeModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
 
-showModal.addEventListener('click', () => {
-    modal.classList.remove('hidden');
+showModalbtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        openModalFunction()
+    });
 });
 
 closeModal.addEventListener('click', () => {
-    modal.classList.add('hidden');
+    closeModalFunction()
 });
+
+overlay.addEventListener('click', () => {
+    closeModalFunction()
+});
+
+function closeModalFunction() {
+    overlay.classList.add('hidden');
+    modal.classList.add('hidden');
+};
+
+function openModalFunction() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
